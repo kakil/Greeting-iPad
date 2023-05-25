f8ebed5b8a8261a160100b574b7da38a65d6ab86
Kitwana Akil <kakil@me.com>
2023-05-25 11:43:06 -0400

    Initial Commit

---
+++ Greeting-iPad/Greeting_iPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Greeting_iPadApp.swift
+//  Greeting-iPad
+//
+//  Created by Kitwana Akil on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Greeting_iPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
