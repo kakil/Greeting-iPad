25066ac29935f255328c65525e94ffee580f351c
Kitwana Akil <kakil@me.com>
2023-05-25 12:50:11 -0400

    Initial commit of completed project code.

--- Greeting-iPad/ContentView.swift
+++ Greeting-iPad/ContentView.swift
@@ -7,15 +7,102 @@
 
 import SwiftUI
 
+func getCurrentTime() -> String {
+    
+    let formatter = DateFormatter()
+    
+    formatter.timeStyle = .short
+    
+    let dateString = formatter.string(from: Date())
+    
+    return dateString
+}
+
+func getCurrentHour() -> String {
+    
+    let formatter = DateFormatter()
+    
+    formatter.dateFormat = "HH"
+    
+    let hourString = formatter.string(from: Date())
+    
+    return hourString
+}
+
+func getCurrentMinutes() -> String {
+    
+    let dateElement = Calendar.current.dateComponents([.minute], from: Date())
+    
+    let minutesString = dateElement.minute ?? 0
+    
+    return String(minutesString)
+}
+
+func chooseGreeting(for hour: Int) -> String {
+    
+    let noon = 12
+    let startOfEvening = 17
+    let startOfNight = 20
+    let midnight = 0
+    
+    
+    if (hour > midnight && hour < noon) {
+        return "Good Morning!"
+    } else if ( hour >= noon && hour < startOfEvening) {
+        return "Good Afternoon!"
+    } else if ( hour > 17 && hour < 20 ) {
+        return "Good Evening!"
+    } else {
+        return "Good Night!"
+    }
+    
+    //return String("Good Afternoon!")
+}
+
+func chooseIcon(for hour: Int) -> String {
+    
+    let startOfNight = 0
+    let startOfSunrise = 5
+    let startOfMorning = 8
+    let startOfSunset = 18
+    
+    if (hour >= startOfNight && hour < startOfSunrise) {
+        return "moon.fill"
+    } else if ( hour >= startOfSunrise && hour < startOfMorning) {
+        return "sunrise.fill"
+    } else if ( hour >= startOfMorning && hour < startOfSunset) {
+        return "sun.fill"
+    } else {
+        return "sunset.fill"
+    }
+    
+}
+
 struct ContentView: View {
+    
+    let currentTime = getCurrentTime()
+    
+    let currentHour = getCurrentHour()
+    
+    let currentMinute = getCurrentMinutes()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        let greeting = chooseGreeting(for: Int(currentHour)!)
+        
+        VStack (spacing: 50) {
+            Text("Current Time:")
+            Text(currentTime)
+            Text(greeting)
+            Image(systemName: chooseIcon(for: 1))
+//            Text("Current Hour:")
+//            Text(currentHour)
+//            Text("Current Minute:")
+//            Text(currentMinute)
         }
         .padding()
+        .font(.title)
+        .fontWeight(.heavy)
     }
 }
 
